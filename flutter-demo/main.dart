2490c86f811817dacd2c661398d12bfd14927859
Atikur Rabbi <msawon94@gmail.com>
2023-01-16 22:44:43 +0600

    update

---
+++ flutter-demo/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExampleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hey there, boo!'),
+      ),
+      body: Center(
+        child: Text(
+          'You are pretty okay.',
+        ),
+      ),
+    );
+  }
+}
